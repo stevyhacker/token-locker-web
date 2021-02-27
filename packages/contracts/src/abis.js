5b1141c371094f72c846770b4aaa9e6876650db3
Stevan Bogosavljevic <stevyhacker@gmail.com>
2021-02-27 15:37:45 +0100

    Add token locker smart contract abi

--- packages/contracts/src/abis.js
+++ packages/contracts/src/abis.js
@@ -1,9 +1,11 @@
 import erc20Abi from "./abis/erc20.json";
 import ownableAbi from "./abis/ownable.json";
+import hodlTokensAbi from "./abis/HodlTokens.json";
 
 const abis = {
   erc20: erc20Abi,
   ownable: ownableAbi,
+  hodlTokens: hodlTokensAbi
 };
 
 export default abis;
