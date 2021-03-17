c68f67402c2c2dde1e91bf54559cdb375814acd9
Stevan Bogosavljevic <stevyhacker@gmail.com>
2021-03-17 14:56:47 +0100

    Add Deposit component to use in a modal on Deposit btn click

--- packages/react-app/src/components/sections/Deposit.js
+++ packages/react-app/src/components/sections/Deposit.js
@@ -6,8 +6,10 @@ import Button from '../elements/Button';
 import {useQuery} from "@apollo/react-hooks";
 import GET_TRANSFERS from "../../graphql/subgraph";
 import useWeb3Modal from '../../hooks/useWeb3Modal';
+import {addresses, abis} from "@project/contracts";
+import {getDefaultProvider} from "@ethersproject/providers";
+import {Contract} from "@ethersproject/contracts";
 import Modal from "../elements/Modal";
-import Deposit from "./Deposit";
 
 const propTypes = {
   ...SectionProps.types
@@ -17,7 +19,18 @@ const defaultProps = {
   ...SectionProps.defaults
 }
 
-function Hero(
+async function readOnChainData() {
+  // Should replace with the end-user wallet, e.g. Metamask
+  const defaultProvider = getDefaultProvider();
+  // Create an instance of an ethers.js Contract
+  // Read more about ethers.js on https://docs.ethers.io/v5/api/contract/contract/
+  const ceaErc20 = new Contract(addresses.ceaErc20, abis.erc20, defaultProvider);
+  // A pre-defined address that owns some CEAERC20 tokens
+  const tokenBalance = await ceaErc20.balanceOf("0x3f8CB69d9c0ED01923F11c829BaE4D9a4CB6c82C");
+  console.log({tokenBalance: tokenBalance.toString()});
+}
+
+function Deposit(
   {
     className,
     topOuterDivider,
@@ -113,15 +126,16 @@ function Hero(
                 <ButtonGroup>
                   <WalletButton provider={provider} loadWeb3Modal={loadWeb3Modal}
                                 logoutOfWeb3Modal={logoutOfWeb3Modal}/>
+
+                  <Button onClick={() => readOnChainData()}>
+                    Read On-Chain Balance
+                  </Button>
                 </ButtonGroup>
 
                 <ButtonGroup>
                   <Button tag="a" color="primary" wideMobile aria-controls="video-modal"
                           onClick={openModal}>
                     Deposit
-                  </Button>
-                  <Button tag="a" color="dark" wideMobile href="https://github.com/cruip/open-react-template/">
-                    Withdraw
                   </Button>
                 </ButtonGroup>
               </div>
@@ -139,7 +153,7 @@ function Hero(
   );
 }
 
-Hero.propTypes = propTypes;
-Hero.defaultProps = defaultProps;
+Deposit.propTypes = propTypes;
+Deposit.defaultProps = defaultProps;
 
-export default Hero;
+export default Deposit;
